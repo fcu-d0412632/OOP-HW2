2a856817731f35a616921e3cb346f63ca24f5a15
user <user@FE02>
2016-10-14 09:44:12 +0800

    更新

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -8,12 +8,17 @@ public class StringComparator {
 
 		Scanner scanner = new Scanner(System.in);
 		
+		int flag = 1;
+		
+		/*
 		System.out.print("Enter a string 1: ");
         String string1 = scanner.next();
         
         System.out.print("Enter a string 2: ");
         String string2 = scanner.next();
+        */
         
+		/*
         if (string1.equals(string2))
         {
         	System.out.print("The two strings are the same");
@@ -22,6 +27,26 @@ public class StringComparator {
         {
         	System.out.print("The two strings are not the same");
         }
+        */
+        
+        do{
+        	System.out.print("Enter a string 1: ");
+            String string1 = scanner.next();
+            
+            System.out.print("Enter a string 2: ");
+            String string2 = scanner.next();
+        	
+        	if (string1.equals(string2))
+            {
+            	System.out.print("The two strings are the same");
+            	flag = 0;
+            }
+            else
+            {
+            	System.out.print("The two strings are not the same,plase again\n");
+            	flag = 1;
+            }
+        }while(flag == 1);
 	}
 
 }
